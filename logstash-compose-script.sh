9fe29864d26b7c7871898ba46c09988ab22c7a3d
adammgourley <61946617+adammgourley@users.noreply.github.com>
2022-03-11 15:26:01 -0500

    Delete logstash-compose-script.sh

--- logstash-compose-script.sh
+++
@@ -1,5 +0,0 @@
-# Change volume permissions for Ubuntu. If you're having permissions issues on Windows or another OS, change permissions on the volume mount point.
-chmod -R 765 /var/lib/docker/volumes/elastic_certs/_data
-
-# Edit the following line if logstash compose file is named something else.
-docker-compose -f logstash-compose-config.yml up -d
